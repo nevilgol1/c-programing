7710d3a93831481871125f729cb72bbb57372a0f
nevilgol1 <nevilgol0@gmail.com>
2025-07-18 16:06:00 +0530

    Add files via upload

---
+++ code/prectical 2/prectical2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int length = 70;
+    int breadth = 90;
+    int area = length*breadth;
+    printf("your area is : %d\n",area);
+    int perimeter = 2*(length+breadth);
+    printf("your perimeter  is : %d\n",perimeter);
+    printf("this program is devoloped by nevil gol 25TCESB9");
+    return 0;
+
+}
---
+++ turboc/FRIST.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+  void main()
+     {
+       clrscr();
+	printf("hello  everyone\n");
+	printf("nevil gol\n");
+	printf("rajkot\n");
+	printf("cspit\n");
+	printf("ce\n")
+	x;
+	getch();
+     }
