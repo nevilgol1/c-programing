b516ab0b6a576be3c57c03caac91afd968cbc4e8
nevil gol <nevilgol0@gmail.com>
2025-08-03 13:40:45 +0530

    Add files via upload

---
+++ code/prectical9.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+  float se,t,e;
+  printf("enter your shopping amount:");
+  scanf("%f",&se);
+  if(se>=5000){
+    t=(se*20)/100;
+    e=se-t;
+    printf("your totle amount is:%.2f",e);
+  }
+
+  else if(1000<se<5000){t=(se*10)/100; e=se-t;
+  printf("your totle amount is:%.2f",e);}
+  else{printf("no discount");}
+  printf("\nthis program is done by nevil gol,25TCESB9");
+  }
+
